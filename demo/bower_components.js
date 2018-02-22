4e3c09b1c320c6b6d96b5a71dd4ed72740e62a50
Niels Holt <niels@steenbuchholt.dk>
2018-02-22 16:45:34 +0100

    Using fcoo/modernizr-mouse-events

--- demo/bower_components.js
+++ demo/bower_components.js
@@ -13581,3 +13581,114 @@ if (typeof define === 'function' && define.amd) {
     }
 
 }));
+
+;
+/****************************************************************************
+	modernizr-javascript.js, 
+
+	(c) 2016, FCOO
+
+	https://github.com/FCOO/modernizr-javascript
+	https://github.com/FCOO
+
+****************************************************************************/
+
+(function ($, window, document, undefined) {
+	"use strict";
+	
+	var ns = window;
+
+    //Extend the jQuery prototype
+    $.fn.extend({
+        modernizrOn : function( test ){ 
+            return this.modernizrToggle( test, true ); 
+        },
+
+        modernizrOff: function( test ){ 
+            return this.modernizrToggle( test, false ); 
+        },
+        
+        modernizrToggle: function( test, on ){ 
+		if ( on === undefined )
+            return this.modernizrToggle( test, !this.hasClass( test ) );
+
+            on = !!on; //on => Boolean
+            return this.toggleClass( test, on ).toggleClass( 'no-' + test, !on );
+        }
+    });
+
+
+    //Add methods to window = works on <html>
+    ns.modernizrOn  = function( test ){ ns.modernizrToggle( test, true ); };
+
+    ns.modernizrOff = function( test ){ ns.modernizrToggle( test, false ); };
+
+    ns.modernizrToggle = function( test, on ){ $('html').modernizrToggle( test, on ); };
+
+}(jQuery, this, document));
+;
+/****************************************************************************
+	modernizr-mouse-events.js,
+
+	(c) 2018, FCOO
+
+	https://github.com/FCOO/modernizr-mouse-events
+	https://github.com/FCOO
+*****************************************************************************/
+(function ($, window/*, document, undefined*/) {
+	"use strict";
+
+    /*
+    Create a Modernizr-test named 'mouse' to detect if there are a mouse-device
+    Solution by http://stackoverflow.com/users/1701813/hacktisch
+    Mouse devices (also touch screen laptops) first fire mousemove before they can fire touchstart and mouse is set to TRUE.
+    Touch devices (also for instance iOS which fires mousemove) FIRST fire touchstart upon click, and then mousemove.
+    That is why mouse will be set to FALSE.
+
+    Create a Modernizr-test named 'mouse-hover' to mark if hover "events" is fired. Can be use to prevent :hover {...} css to fail on touch devices
+    */
+
+	/******************************************
+	Initialize/ready
+	*******************************************/
+	$(function() {
+        var mouseTest         = 'mouse',
+            mouseHoverTest    = 'mouse-hover',
+            mouseEventPostfix = '.modernizr.mouse.events',
+            hasModernizr = !!window.Modernizr,
+            hasTouchEventsTest = hasModernizr && (jQuery.type( window.Modernizr['touchevents'] ) === "boolean"),
+            hasTouchEvents = hasTouchEventsTest ? window.Modernizr['touchevents'] : true;
+
+        if (hasModernizr)
+            window.Modernizr.addTest(mouseTest, false);
+        window.modernizrOff(mouseTest);
+
+        //If Modernizr-test "touchevents" is included => use if to set "mouse-hover" else set "mouse-hover" = "mouse"
+        if (hasModernizr)
+            window.Modernizr.addTest( mouseHoverTest, !hasTouchEvents );
+        window.modernizrToggle( mouseHoverTest, !hasTouchEvents );
+
+        $(window)
+            //Check for mouse
+            .bind('mousemove'+mouseEventPostfix,function(){
+                $(window).unbind(mouseEventPostfix);
+                if (hasModernizr)
+                    window.Modernizr[mouseTest] = true;
+                window.modernizrOn(mouseTest);
+                if (!hasTouchEventsTest){
+                    if (hasModernizr)
+                        window.Modernizr.addTest( mouseHoverTest, true );
+                    window.modernizrOn(mouseHoverTest);
+                }
+            })
+            .bind('touchstart'+mouseEventPostfix,function(){
+                $(window).unbind(mouseEventPostfix);
+                if (hasModernizr)
+                    window.Modernizr[mouseTest] = false;
+                window.modernizrOff(mouseTest);
+            });
+    });
+
+    //******************************************
+
+}(jQuery, this, document));
